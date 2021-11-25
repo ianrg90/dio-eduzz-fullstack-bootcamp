f0aed1d996de701e3b63ffbe5155e4f513352fce
ianrg90 <iangladulich@gmail.com>
2021-11-25 14:45:53 -0300

    URL shortner using express, node and mongodb

---
+++ url-shortner/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://testuser:KvyUnMlLUCm9LdLr@cluster0.dsvpt.mongodb.net/urls?retryWrites=true&w=majority',
+}
